159826398575cf0b5787854259ad07a477e1334a
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-01-17 20:39:31 +0100

    shoot function done (not working properly)

--- main.py
+++ main.py
@@ -37,5 +37,34 @@ def config_player():
     # Sistema de vides
     info.set_life(3)
 
+def shoot():
+    """
+    Genera un projectil desde la possició del jugador
+    """
+    # Només disparem si el jugador existeix
+    if my_player:
+        # Creem el projectil (placeholder momentani)
+        projectile = sprites.create_projectile_from_sprite(img("""
+        . . . . .
+        . . 5 . .
+        . 5 5 5 .
+        . . 5 . .
+        . . . . .
+        """), my_player, 0, 0) # Velocitat inicial: 0
+
+        # Lògica per disparar cap on mirem
+        if controller.dx() == 0 and controller.dy() == 0:
+            projectile.vx = 100 # direcció projectil per defecte: dreta
+        else:
+            # Velocitat del projectil segons velocitat del jugador
+            projectile.vx = controller.dx() * 2
+            projectile.vy = controller.dy() * 2
+        
+        # Destruïm el projectil un cop surt de la pantalla
+        projectile.set_flag(SpriteFlag.DESTROY_ON_WALL, True)
+    
+    # Vinculem al botó A la funció
+    controller.A.on_event(ControllerButtonEvent.PRESSED, shoot)
+
 # CRIDA INICIAL
 config_player()
