853d98546a1bb4c6516f1f957865a41679613f30
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-01-17 21:09:42 +0100

    on_update_game function done, fixes shoot function problems

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # VARIABLES GLOBALS
 my_player: Sprite = None
 
-# FUNCIONS DE CONFIGURACIÓ
-def config_player():
+# Variables d'estat de d'apuntament (dreta per defecte)
+facing_x: number = 1
+facing_y: number = 0
+
+# Constant de velociat del projectil
+projectile_speed = 200
+
+# FUNCIÓ DE CONFIGURACIÓ
+def setup_player():
     """
     Crea el sprite del jugador, defineix les seves físiques i estableix vides.
     """
@@ -37,10 +44,41 @@ def config_player():
     # Sistema de vides
     info.set_life(3)
 
-def shoot():
+# BUCLE D'ACTUALITZACIÓ
+def on_game_update():
+    """
+    Aquesta funció s'executa a cada frame del joc
+    """
+    global facing_x, facing_y
+
+    # Detectem si el jugador s'està movent
+    if controller.dx() != 0 or controller.dy() != 0:
+        # Si ens movem, actualitzem la direcció on mirem.
+
+        # Gestió eix X
+        if controller.dx() > 0: facing_x = 1
+        elif controller.dx() < 0: facing_x = -1
+        else: facing_x = 0
+
+        # Gestió eix Y
+        if controller.dy() > 0: facing_y = 1
+        elif controller.dy() <0: facing_y = -1
+        else: facing_y = 0
+
+        # Gestió de diagonals (excepció)
+        if facing_x !=0 and facing_y !=0:
+            pass
+
+# Vinculem la funció del bucle al joc
+game.on_update(on_game_update)
+
+# SISTEMA DE COMBAT
+def shoot_projectile():
     """
     Genera un projectil desde la possició del jugador
     """
+    global my_player, facing_x, facing_y
+
     # Només disparem si el jugador existeix
     if my_player:
         # Creem el projectil (placeholder momentani)
@@ -53,18 +91,19 @@ def shoot():
         """), my_player, 0, 0) # Velocitat inicial: 0
 
         # Lògica per disparar cap on mirem
-        if controller.dx() == 0 and controller.dy() == 0:
-            projectile.vx = 100 # direcció projectil per defecte: dreta
+        if facing_x == 0 and facing_y == 0:
+            projectile.vx = projectile_speed # direcció projectil per defecte: dreta
         else:
             # Velocitat del projectil segons velocitat del jugador
-            projectile.vx = controller.dx() * 2
-            projectile.vy = controller.dy() * 2
+            projectile.vx = facing_x * projectile_speed
+            projectile.vy = facing_y * projectile_speed
         
         # Destruïm el projectil un cop surt de la pantalla
         projectile.set_flag(SpriteFlag.DESTROY_ON_WALL, True)
-    
-    # Vinculem al botó A la funció
-    controller.A.on_event(ControllerButtonEvent.PRESSED, shoot)
 
-# CRIDA INICIAL
-config_player()
+# EXECUCIÓ
+setup_player()
+
+# Vinculem al botó A la funció shoot_projectile
+controller.A.on_event(ControllerButtonEvent.PRESSED, shoot_projectile)
+
--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 //  VARIABLES GLOBALS
 let my_player : Sprite = null
-//  FUNCIONS DE CONFIGURACIÓ
-function config_player() {
+//  Variables d'estat de d'apuntament (dreta per defecte)
+let facing_x = 1
+let facing_y = 0
+//  Constant de velociat del projectil
+let projectile_speed = 200
+//  FUNCIÓ DE CONFIGURACIÓ
+function setup_player() {
     /** Crea el sprite del jugador, defineix les seves físiques i estableix vides. */
     
     //  Placeholder temporal per my_player
@@ -31,5 +36,71 @@ function config_player() {
     info.setLife(3)
 }
 
-//  CRIDA INICIAL
-config_player()
+//  BUCLE D'ACTUALITZACIÓ
+//  Vinculem la funció del bucle al joc
+game.onUpdate(function on_game_update() {
+    /** Aquesta funció s'executa a cada frame del joc */
+    
+    //  Detectem si el jugador s'està movent
+    if (controller.dx() != 0 || controller.dy() != 0) {
+        //  Si ens movem, actualitzem la direcció on mirem.
+        //  Gestió eix X
+        if (controller.dx() > 0) {
+            facing_x = 1
+        } else if (controller.dx() < 0) {
+            facing_x = -1
+        } else {
+            facing_x = 0
+        }
+        
+        //  Gestió eix Y
+        if (controller.dy() > 0) {
+            facing_y = 1
+        } else if (controller.dy() < 0) {
+            facing_y = -1
+        } else {
+            facing_y = 0
+        }
+        
+        //  Gestió de diagonals (excepció)
+        if (facing_x != 0 && facing_y != 0) {
+            
+        }
+        
+    }
+    
+})
+//  SISTEMA DE COMBAT
+//  EXECUCIÓ
+setup_player()
+//  Vinculem al botó A la funció shoot_projectile
+controller.A.onEvent(ControllerButtonEvent.Pressed, function shoot_projectile() {
+    let projectile: Sprite;
+    /** Genera un projectil desde la possició del jugador */
+    
+    //  Només disparem si el jugador existeix
+    if (my_player) {
+        //  Creem el projectil (placeholder momentani)
+        projectile = sprites.createProjectileFromSprite(img`
+        . . . . .
+        . . 5 . .
+        . 5 5 5 .
+        . . 5 . .
+        . . . . .
+        `, my_player, 0, 0)
+        //  Velocitat inicial: 0
+        //  Lògica per disparar cap on mirem
+        if (facing_x == 0 && facing_y == 0) {
+            projectile.vx = projectile_speed
+        } else {
+            //  direcció projectil per defecte: dreta
+            //  Velocitat del projectil segons velocitat del jugador
+            projectile.vx = facing_x * projectile_speed
+            projectile.vy = facing_y * projectile_speed
+        }
+        
+        //  Destruïm el projectil un cop surt de la pantalla
+        projectile.setFlag(SpriteFlag.DestroyOnWall, true)
+    }
+    
+})
